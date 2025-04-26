1634cb82c0928f8051389adc7f6e1627b3bcebee
igaryakqwe <igor.novykov@gmail.com>
2025-04-26 18:54:23 +0300

    fix: providers

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,11 +1,17 @@
-import type { PropsWithChildren } from "react";
-import { NuqsAdapter } from "nuqs/adapters/next/app";
+import { NuqsAdapter } from "nuqs/adapters/react";
+import { type PropsWithChildren } from "react";
 
 import AuthProvider from "@/providers/auth-provider";
 import QueryProvider from "@/providers/query-provider";
 
 const Providers = ({ children }: PropsWithChildren) => {
-  return <NuqsAdapter>{children}</NuqsAdapter>;
+  return (
+    <NuqsAdapter>
+      <QueryProvider>
+        <AuthProvider>{children}</AuthProvider>
+      </QueryProvider>
+    </NuqsAdapter>
+  );
 };
 
 export default Providers;
